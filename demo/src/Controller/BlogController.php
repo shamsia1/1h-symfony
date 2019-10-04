3ef22a7cca90e3e62177c19dd26c1c6f6a911147
shamsia1 <alimishamsya@gmail.com>
2019-10-04 15:02:59 +0200

    project symfony

---
+++ demo/src/Controller/BlogController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+//use App\Form\ArticleType
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index()
+    {
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $articles = $repo->findAll();
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function  home() {
+        return $this->render('blog/home.html.twig');
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit", name="blog_edit")
+     */
+    public function form(Article $article = null, Request $request, ObjectManager $manager){
+        if(!$article){
+             $article = new Article();
+
+        }
+
+        $form = $this->createFormBuilder($article)
+                    ->add('title', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "Titre de l'article"
+                        ]
+                    ] )
+                    ->add('content', TextareaType::class, [
+                        'attr' => [
+                            'placeholder' => "content de l'article"
+                        ]
+                    ])
+                    ->add('image', TextType::class, [
+                        'attr' => [
+                            'placeholder' => "image de l'article"
+                        ]
+                    ])
+                    ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            if(!$article->getId()) {
+                $article->setCreatedAt(new \DateTime());
+            }
+            $manager->persist($article);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $article->getId()
+            ]);
+        }
+        return $this ->render('blog/create.html.twig', [
+            'formArticle' => $form->createView(),
+            'editMode' => $article->getId() !== null
+        ]);
+    }
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show($id){
+        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $article = $repo->find($id);
+        return $this->render('blog/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+}
+// $form->$this->createForm(ArticleType::class, $article)
